f6024912acd2c564a0bead43ecb29470f82b3ee1
Lucas Cabrera <106570221+LucasCabrera94@users.noreply.github.com>
2024-09-30 20:52:32 -0300

    Autos

--- Program.cs
+++ Program.cs
@@ -1,1 +1,58 @@
-﻿
+﻿Vehiculo auto = new Vehiculo("BMW", "M4", 2024);
+Console.WriteLine(auto.MostrarEstado());
+ 
+ class Vehiculo
+{
+    //propiedades o atributos:
+
+    protected string? Marca;  //protected porque solo "yo"  y algunos podemos acceder.
+    protected string? Modelo;
+    protected int? Anio;
+    protected int? Velocidad = 0;
+    protected bool Contacto = false;
+    //Metodos:
+
+    public Vehiculo(string Marca, string Modelo, int Anio){  //Constructor
+        this.Marca = Marca;
+        this.Modelo = Modelo;
+        this.Anio = Anio;
+    }
+
+    
+    public string Encender()
+    {   
+        Contacto = true;
+        return "El vehículo esta encendio";
+    }
+
+    public string apagar()
+    {   
+        Contacto = false;
+        return "El vehículo se apagó correctamente.";
+    }
+    public void Acelerar()
+    {
+        Velocidad = Velocidad +1;
+    }
+
+    public void Desacelerar()
+    {
+        Velocidad = Velocidad -1;
+    }
+
+    public string Detener()
+    {
+        if (Velocidad <= 10){
+            Velocidad = 0;
+            return "El vehículo se detuvo";
+        }
+        else{
+            return "El vehículo se rompio todo";
+        }
+    }
+
+    public string MostrarEstado()
+    {
+        return $"Marca:{Marca}\nModelo: {Modelo}\nAño: {Anio}\nVelocidad: {Velocidad}\n¿Está encendido?: {Contacto}"; 
+    }
+}
